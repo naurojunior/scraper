6f0944f0db927f80f404cbe3d7c3762dea4290ae
Nauro <nauro@outlook.com>
2022-12-01 01:27:43 -0300

    Fix change in status

--- main.py
+++ main.py
@@ -10,35 +10,51 @@ company_url = config['DEFAULT']['CompanyURL']
 api_token = config['DEFAULT']['APIToken']
 chat_id = config['DEFAULT']['ChatId']
 
+previous_status = None
+current_status = None
 
 
-def send_message():
+def send_message(message):
     print("Send Message")
 
-    message = 'Água voltou ou algo mudou'
-    apiURL = f'https://api.telegram.org/bot{api_token}/sendMessage'
+    api_url = f'https://api.telegram.org/bot{api_token}/sendMessage'
 
     try:
-        response = requests.post(apiURL, json={'chat_id': chat_id, 'text': message})
+        response = requests.post(api_url, json={'chat_id': chat_id, 'text': message})
         print(response.text)
     except Exception as e:
         print(e)
 
 
-
 while True:
 
-    page = requests.get(company_url)
+    page = requests.get(company_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(id="statusModal")
     box_title = results.find("div", class_="box-titulo")
     status = box_title.find("div").find("div")["style"]
 
-    if "#f51616" not in str(status):
-        print("Changes Found!")
-        send_message()
+    if "#f51616" in str(status):
+        current_status = "broken"
     else:
-        print("Nothing changed, but I'm still alive!")
+        current_status = "working"
+
+    if previous_status is None:
+        previous_status = current_status
+
+    if previous_status != current_status:
+        print("Changes found! Current status:" + current_status)
+
+        if current_status == "broken":
+            current_message = "Interrupção no serviço de água"
+        else:
+            current_message = "Serviço de água voltou a funcionar"
+
+        send_message(current_message)
+    else:
+        print("Nothing changed. Still alive. Current status:" + current_status)
+
+    previous_status = current_status
 
     time.sleep(600)
